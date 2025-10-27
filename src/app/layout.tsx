6452f511ebd03572f7dba06fc7cd6f2faf4f9088
Miguel González Pascual <miguelgp789@gmail.com>
2025-10-27 13:58:56 +0100

    Cambiado el favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,19 +8,7 @@ export const metadata: Metadata = {
     title: "InterCodeView",
     description: "Plataforma de entrevistas técnicas",
     icons: {
-        icon: [
-            {
-                url: "/InterCodeViewLogo_resized.png",
-                sizes: "32x32",
-                type: "image/png",
-            },
-            {
-                url: "/InterCodeViewLogo_resized.png",
-                sizes: "16x16",
-                type: "image/png",
-            },
-        ],
-        apple: "/InterCodeViewLogo_resized.png",
+        icon: "/medalla.ico",
     },
 };
 
